05e996e24d84f4f4ece27f3e60c6189403f4796d
Dede Suryaman <de2suryaman@gmail.com>
2022-01-13 06:53:15 +0700

    merapikan usercontroller

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -8,28 +8,21 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     //
-
     public function login(){
         return 'login';
     }
-
     public function login_check(){
         return json_encode(array('status' => 'login check') ,true);
     }
-	
-	
 	public function register(){
         return 'register';
     }
-	
 	public function register_check(){
         return json_encode(array('status' => 'register check') ,true);
     }
-	
 	public function forget(){
         return 'forget password';
     }
-	
 	public function reset_password(){
         return json_encode(array('status' => 'reset password') ,true);
     }
